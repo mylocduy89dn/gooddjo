26eaa5e3b0d4f876a2087000df98fd6fbb94d8d3
mylocduy89dn <139839626+mylocduy89dn@users.noreply.github.com>
2025-01-01 07:48:20 +0700

    Update EmployeeDAOImpl.java
    ff123

--- 19JSP02/src/jsp/examples/bt3/EmployeeDAOImpl.java
+++ 19JSP02/src/jsp/examples/bt3/EmployeeDAOImpl.java
@@ -17,7 +17,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 		Scanner scanner = new Scanner(System.in);
 		int employeeNo = scanner.nextInt();
 		employees = new Em11ployee[employeeNo];
-		for (int i = 0; i < employeeNo; i++) {
+		for (int i = 0; i < em123ployeeNo; i++) {
 			System.out.println("Please input the information of employee[" + (i + 1) + "] : ");
 			System.out.println("Name : ");
 			String name = scanner.next();
