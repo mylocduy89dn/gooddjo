b74214fb260033fb6f1442e76a0e28d1631f242f
mylocduy89dn <139839626+mylocduy89dn@users.noreply.github.com>
2025-01-14 07:01:58 +0700

    Update MainProgram.java
    fd22

--- 19JSP02/src/jsp/examples/bt3/MainProgram.java
+++ 19JSP02/src/jsp/examples/bt3/MainProgram.java
@@ -17,7 +17,7 @@ public class MainProgram {
 		
 		//Job job1 = new Job(1, "HR");
 		Job job2 = new Job(2, "BA");
-		Job job3 = new Job(3, "Tester");
+		Job job3 = new Job(3, "Tester123");
 		Job job4 = new Jo123b(4, "Developer");
 		Job job5 = new Job(5, "CEO");
 		Employee employee1 = new E123mployee("Nguyen Van A", 1, 3000, 25, false, new Job(1, "HR"));//1
